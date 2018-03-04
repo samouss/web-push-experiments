cb533e6d64a1b480ad51f02a0fdbd7238f7bb12d
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-03-04 17:30:02 +0100

    feat(server): trigger notifications

--- server/index.js
+++ server/index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const webPush = require('web-push');
 
 const port = process.env.PORT || 3000;
+const publicKey = process.env.PUSH_PUBLIC_KEY || '';
 const privateKey = process.env.PUSH_PRIVATE_KEY || '';
 
 const state = new Set();
 const server = express();
+
+// prettier-ignore
+webPush.setVapidDetails(
+  'mailto:hello@world.com',
+  publicKey,
+  privateKey
+);
 
 server.use(cors());
 server.use(bodyParser.json());
@@ -18,6 +27,25 @@ server.post('/subscriptions', (req, res) => {
   res.status(201).end();
 });
 
+// Trigger notifications
+server.post('/notifications', (req, res) => {
+  const payload = JSON.stringify(req.body);
+  const requests = [...state].map(subsription =>
+    webPush.sendNotification(subsription, payload)
+  );
+
+  Promise.all(requests)
+    .then(() => {
+      res.status(201).end();
+    })
+    .catch(error => {
+      res.status(400).json({
+        error,
+      });
+    });
+});
+
+// Launch the server
 server.listen(port, () => {
   console.log(`Server listen on port: ${port}`);
 });
