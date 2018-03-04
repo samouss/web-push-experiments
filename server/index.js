23682677ecd70210627933a396f02c1670ae3a3d
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-03-04 10:00:58 +0100

    feat(server): register push subscription

--- server/index.js
+++ server/index.js
@@ -1,7 +1,22 @@
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const port = process.env.PORT || 3000;
+const privateKey = process.env.PUSH_PRIVATE_KEY || '';
+
+const state = new Set();
 const server = express();
+
+server.use(cors());
+server.use(bodyParser.json());
+
+// Register a subscription
+server.post('/subscriptions', (req, res) => {
+  state.add(req.body);
+
+  res.status(201).end();
+});
 
 server.listen(port, () => {
   console.log(`Server listen on port: ${port}`);
